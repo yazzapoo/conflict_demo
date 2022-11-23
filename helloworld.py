4a3e114cf827c9a055a4b6f0e1f952d022b8b72b
Yasmine Lelaind <yasminelelaind@gmail.com>
2022-11-22 20:25:23 -0800

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
@@ -8,14 +8,4 @@ def main():
 
 
 main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
 
-
-def main():
-    hello_world('NYC')
->>>>>>> 75442bb60e9a753d32e1cfeb06c4bd7fb53faf6a
-
-
-main()
